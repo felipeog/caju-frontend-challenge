28aefb7a3dc475328710a9898e4ad5bc4d3a8e0a
felipeog <17603069+felipeog@users.noreply.github.com>
2023-06-22 20:22:33 -0300

    chore(theme): set theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,21 @@
 import { SWRConfig } from "swr";
+import { ThemeProvider } from "@mui/material";
 
 import { Home } from "@/pages/Home";
+import { theme } from "@/theme";
 
 export function App() {
   return (
-    <SWRConfig
-      value={{
-        revalidateIfStale: false,
-        revalidateOnFocus: false,
-        revalidateOnReconnect: false,
-      }}
-    >
-      <Home />
-    </SWRConfig>
+    <ThemeProvider theme={theme}>
+      <SWRConfig
+        value={{
+          revalidateIfStale: false,
+          revalidateOnFocus: false,
+          revalidateOnReconnect: false,
+        }}
+      >
+        <Home />
+      </SWRConfig>
+    </ThemeProvider>
   );
 }
