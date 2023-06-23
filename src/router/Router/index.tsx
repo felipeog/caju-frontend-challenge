3b97869724a0135946d9bcb9d18062cf5b39441e
felipeog <17603069+felipeog@users.noreply.github.com>
2023-06-22 22:13:03 -0300

    chore(router): add not found page

--- src/router/Router/index.tsx
+++ src/router/Router/index.tsx
@@ -5,6 +5,7 @@ import { Home } from "@/pages/Home";
 import { Joke } from "@/pages/Joke";
 import { Jokes } from "@/pages/Jokes";
 import { JokeSearch } from "@/pages/JokeSearch";
+import { NotFound } from "@/pages/NotFound";
 import { RandomJoke } from "@/pages/RandomJoke";
 import { Layout } from "@/router/Layout";
 
@@ -18,7 +19,7 @@ function Router() {
         <Route path="joke-search" element={<JokeSearch />} />
         <Route path="joke/:jokeId" element={<Joke />} />
         <Route path="about" element={<About />} />
-        <Route path="*" element={<p>404 - Página não encontrada</p>} />
+        <Route path="*" element={<NotFound />} />
       </Route>
     </Routes>
   );
