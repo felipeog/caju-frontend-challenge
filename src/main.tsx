b00526f29cc87016c95a51cca0a7e8089f171dc7
felipeog <17603069+felipeog@users.noreply.github.com>
2023-06-24 13:41:29 -0300

    chore(header): use tabs

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import { initSentry } from "@/services/sentry";
-
-import { App } from "./App";
-import "./index.css";
+import { App } from "@/App";
+import "@/index.css";
 
 initSentry();
 
--- src/router/Header/index.tsx
+++ src/router/Header/index.tsx
@@ -1,29 +1,38 @@
-import { Link } from "react-router-dom";
+import { useMemo } from "react";
+import { Link, matchPath, useLocation } from "react-router-dom";
+import { Tab, Tabs } from "@mui/material";
+
+const ROUTE_PATTERNS = [
+  "/",
+  "/jokes",
+  "/random",
+  "/search",
+  "/about",
+  "/starred",
+];
 
 function Header() {
+  const location = useLocation();
+
+  const currentTab = useMemo(() => {
+    const currentPattern = ROUTE_PATTERNS.find((pattern) =>
+      matchPath(pattern, location.pathname)
+    );
+    const match = matchPath(currentPattern ?? "", location.pathname);
+
+    return match ? match?.pattern?.path : false;
+  }, [location.pathname]);
+
   return (
     <nav className="Header">
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-
-        <li>
-          <Link to="/jokes">Jokes</Link>
-        </li>
-
-        <li>
-          <Link to="/random-joke">Random joke</Link>
-        </li>
-
-        <li>
-          <Link to="/joke-search">Joke search</Link>
-        </li>
-
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-      </ul>
+      <Tabs value={currentTab}>
+        <Tab label="Home" value="/" to="/" component={Link} />
+        <Tab label="Jokes" value="/jokes" to="/jokes" component={Link} />
+        <Tab label="Random" value="/random" to="/random" component={Link} />
+        <Tab label="Search" value="/search" to="/search" component={Link} />
+        <Tab label="About" value="/about" to="/about" component={Link} />
+        <Tab label="Starred" value="/starred" to="/starred" component={Link} />
+      </Tabs>
     </nav>
   );
 }
