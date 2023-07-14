2b79084deaecbac4386f5c4a20d6feaa5f42bd63
felipeog <17603069+felipeog@users.noreply.github.com>
2023-07-14 10:54:00 -0300

    Configure Amplitude

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import { initSentry } from "@/services/sentry";
+import { initAmplitude } from "@/services/amplitude";
 import { App } from "@/App";
 
 initSentry();
+initAmplitude();
 
 const root = document.getElementById("root") as HTMLElement;
 
---
+++ src/services/amplitude.ts
@@ -0,0 +1,7 @@
+import * as amplitude from "@amplitude/analytics-browser";
+
+export function initAmplitude() {
+  amplitude.init("a4a6d13c71c78d5f51d39c3a810b11ee");
+}
+
+export { amplitude };
